035a3cfadae95b8c7852cbd9f658444e196aa13c
Roshanak <rosh.agahi@gmail.com>
2021-07-04 16:35:33 -0700

    fetch backend json

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const endPoint = "http://127.0.0.1:3000/api/v1/flashcards"
+
 document.addEventListener('DOMContentLoaded', () => {
-  console.log("loaded");
+  fetch(endPoint)
+  .then(response => response.json())
+  .then(flashcards => {
+    console.log(flashcards);
+  })
+
 })
