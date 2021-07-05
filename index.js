31421f268fe864709a37f480d9594e73add4bca7
Roshanak <rosh.agahi@gmail.com>
2021-07-04 17:00:15 -0700

    add markup to render each flashcard

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ function getFlashcards() {
   fetch(endPoint)
   .then(response => response.json())
   .then(flashcards => {
-    console.log(flashcards);
+    flashcards.data.forEach(flashcard => {
+      const flashcardMarkup =
+      <div data-id=${flashcard.id}>
+        <h2>${flashcard.term}</h2>
+        <h2>${flashcard.subject}</h2>
+        <p>${flascard.definition}</p>
+        <br>
+    });
   })
 }
