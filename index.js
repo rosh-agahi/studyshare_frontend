fec3165f217760b1b997735a4b9fea40765fbbf9
Roshanak <rosh.agahi@gmail.com>
2021-07-05 17:38:46 -0700

    function for form handler

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const endPoint = "http://127.0.0.1:3000/api/v1/flashcards"
 
 document.addEventListener('DOMContentLoaded', () => {
   getFlashcards()
+
+  const createFlashcardForm = document.querySelector("#create_flashcard_form")
+
+  createFlashcardForm.addEventListener("submit", (e) => createFormHandler(e))
 })
+
 
 function getFlashcards() {
   fetch(endPoint)
@@ -21,4 +26,17 @@ function getFlashcards() {
         document.querySelector('#flashcard-container').innerHTML += flashcardMarkup
     })
   })
+}
+
+function createFormHandler(e) {
+  e.preventDefault()
+  const termInput = document.querySelector(#term).value
+  const definitionInput = document.querySelector(#definition).value
+  const subjectId = parseInt(document.querySelector(#subject).value)
+  const userId = parseInt(1)
+  postFetch(termInput, definitionInput, subjectId)
+}
+
+function postFetch(term, definition, subject, user){
+
 }
