5ad1926bfe5157336255769b7867bc212ac2d325
Roshanak <rosh.agahi@gmail.com>
2021-07-17 09:45:51 -0700

    added Subject object

--- js/subjects.js
+++ js/subjects.js
@@ -1,3 +1,10 @@
 class Subject {
-
+  constructor(subject, subjectAttributes) {
+    this.id = subject.id
+    this.name = subjectAttributes.name
+    Subject.all.push(this)
+    console.log(this);
+  }
 }
+
+Subject.all = []
